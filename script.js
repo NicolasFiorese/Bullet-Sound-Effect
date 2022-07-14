54e48ec9bf2110ef61327863837f4cf210a308c9
Nicolas Fiorese <103607839+NicolasFiorese@users.noreply.github.com>
2022-07-14 20:42:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('click', function (e) {
+  var body = document.querySelector('body')
+  var bullet = document.createElement('span')
+  var x = e.offsetX
+  var y = e.offsetY
+  bullet.style.left = x + 'px'
+  bullet.style.top = y + 'px'
+  body.appendChild(bullet)
+
+  var audio = document.getElementById('audio')
+  audio.volume = 0.3
+  audio.play()
+})
